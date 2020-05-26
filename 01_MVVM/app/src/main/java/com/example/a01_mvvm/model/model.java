16fdb2f06713827cec438d2cfd5295b443a07832
jvillarroelc <jvillarroel@megatlon.com.ar>
2020-05-26 00:06:21 -0300

    Se crea la estructura mvvm

---
+++ 01_MVVM/app/src/main/java/com/example/a01_mvvm/model/model.java
@@ -0,0 +1,4 @@
+package com.example.a01_mvvm.model;
+
+public class model {
+}
---
+++ 01_MVVM/app/src/main/java/com/example/a01_mvvm/view/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.a01_mvvm.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.a01_mvvm.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
